daad3d09d2859350d6777fcfd197e6351be36ad3
Mike Price <maprice35@gmail.com>
2013-05-06 22:45:23 -0400

    Added listview

--- src/com/maprice/pointademo/PAActivity.java
+++ src/com/maprice/pointademo/PAActivity.java
@@ -1,22 +1,40 @@
 package com.maprice.pointademo;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PAActivity extends Activity {
-
+public class PAActivity extends ListActivity {
+	 
+	static final String[] FRUITS = new String[] { "Ads", "Analytics", "Billing", "Cloud", "Crash", "Facebook" };
+ 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_pa);
+ 
+		// no more this
+		// setContentView(R.layout.list_fruit);
+ 
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_pa,FRUITS));
+ 
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+ 
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+			    // When clicked, show a toast with the TextView text
+			    Toast.makeText(getApplicationContext(),
+				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+ 
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.pa, menu);
-		return true;
-	}
-
+ 
 }
