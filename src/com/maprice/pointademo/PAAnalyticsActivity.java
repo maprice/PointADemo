e7d76ee1efe7f488e7463e4f00d20aeefd873777
psalazar515 <psalazar_92@hotmail.com>
2014-06-18 20:36:04 -0400

    Analytics
    editted analytics activity to call PointA functions

--- src/com/maprice/pointademo/PAAnalyticsActivity.java
+++ src/com/maprice/pointademo/PAAnalyticsActivity.java
@@ -1,5 +1,7 @@
 package com.maprice.pointademo;
 
+import com.pointa.PointA;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +20,7 @@ public class PAAnalyticsActivity extends Activity{
 		setContentView(R.layout.linear_layout);
 		
 		lin_layout = (LinearLayout)findViewById(R.id.linear_layout);
+		PointA.init();
 		
 		for (int i = 1; i <= 3; i++){
 			b = new Button(this);
@@ -26,10 +29,23 @@ public class PAAnalyticsActivity extends Activity{
 			b.setOnClickListener(new View.OnClickListener(){
 	             public void onClick(View v) {
 	            	 String msg = "Button " + v.getId() + " Pressed";
+	            	 PointA.analytics().send(PAAnalyticsActivity.this, "PAAnalyticsActivity", msg, null);
 	            	 Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show(); 
 	             }
-	        });
+			});
 			lin_layout.addView(b);
 		}
+	}
+	
+	@Override
+	public void onStart(){
+		super.onStart();
+		PointA.analytics().send_onStart(this);
+	}
+	
+	@Override
+	public void onStop(){
+		super.onStop();
+		PointA.analytics().send_onStop(this);
 	}
 }
